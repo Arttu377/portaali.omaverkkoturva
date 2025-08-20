19c2b0ca168444dcc19b6f63ee83172bd24ed7f8
Arttu377 <arttu.simanainen@gmail.com>
2025-08-20 10:43:08 +0300

    Fix TypeScript import and portal build now works

--- src/portal-main.tsx
+++ src/portal-main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async';
-import PortalApp from './PortalApp.tsx'
+import PortalApp from './PortalApp'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(
